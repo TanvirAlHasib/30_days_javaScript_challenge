d9c63220ea336ab9a769dceaf731f762eedf0112
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-07-13 20:32:53 +0600

    day_01 done

---
+++ day_01/index.js
@@ -0,0 +1,52 @@
+// activity 01 - variable declaretion
+// task 01:
+var number = 2;
+console.log(number);
+// task 02:
+let string = "Tanvir Al Hasib";
+console.log(string);
+
+
+// activity 02 - constant declaretion
+// task 03:
+const bolean = true;
+console.log(bolean);
+
+
+// activity 03 - data types
+// task 04:
+let number_variable = 5;
+console.log(typeof(number_variable));
+
+let string_variable = "It is a string";
+console.log(typeof(string_variable));
+
+let bolean_variable = false;
+console.log(typeof(bolean_variable));
+
+let object_variable = {
+    number: 123,
+    name: "tanvir al hasib",
+};
+console.log(typeof(object_variable));
+
+let array_variable = [1, "hello", 2, "bye"];
+console.log(typeof(array_variable));
+
+
+// activity 04 - reasigning variables
+// task 05:
+let assign = 123;
+console.log(assign);
+
+assign = 456;
+console.log(assign);
+
+
+// activity 04 - reasigning variables
+// task 06:
+const variable = 123;
+console.log(variable);
+
+variable = 456;
+console.log(variable);  // reasign is not possible in const variable
